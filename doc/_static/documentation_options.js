a7666a3bfc8841cea5312389c63dff08ffbc0fd8
Stewart Smith <stewart@linux.vnet.ibm.com>
2014-11-24 15:50:05 +1100

    Skiboot documentation as of v6.3-190-g9cae036fafea
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

---
+++ doc/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v6.3-190-g9cae036fafea',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
